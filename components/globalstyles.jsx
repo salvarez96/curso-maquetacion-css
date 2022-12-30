a37cefdf42f00dcf7b6fcc5790fdca373deb0bda
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2022-12-30 17:39:21 -0500

    Clase 5: agregando el header general del blog

--- components/globalstyles.jsx
+++ components/globalstyles.jsx
@@ -6,6 +6,10 @@ const GlobalStyle = createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
   }
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
 `;
 
 export default GlobalStyle;
